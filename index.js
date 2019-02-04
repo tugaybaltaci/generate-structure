988febe890a31875da55487916ac7d18f0e5edf0
Tugay Baltacı <tugaybaltaci@gmail.com>
2019-02-04 11:08:13 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const parser = require('dom-parser');
+
+function CreateTemplate() {}
+
+CreateTemplate.prototype.parse = function(markup) {};
+CreateTemplate.prototype.evaluate = function(script) {};
