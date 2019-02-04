1c1ea5492abed8c8ae69db01f2e087d5c443a015
Tugay Baltacı <tugaybaltaci@gmail.com>
2019-02-04 13:54:03 +0300

    template file name changed

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 
 const Schema = require('./core/Schema');
 
-const tpl = new Schema('./templates/test.scml', 'component-name');
+const tpl = new Schema('./templates/a15-component.scml', 'component-name');
 
 tpl.createAll();
