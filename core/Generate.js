9e4171cea9ff026f93d3f4a511f44775bfc2885f
Tugay Baltacı <tugaybaltaci@gmail.com>
2019-04-29 14:54:29 +0300

    Update Generate.js
    Optimized for a15 page generation

--- core/Generate.js
+++ core/Generate.js
@@ -1,24 +1,22 @@
+/* eslint-disable */
+
 const fs = require('fs');
 const shell = require('shelljs');
-
 const HTMLParser = require('node-html-parser');
 
 const File = require('./File');
 
 class Generate {
-
   constructor(name, template, variables = {}) {
     this.variables = { ...variables, name };
     this.variableTemplate = '%var%';
     this.outputDirectory = null;
 
-    this.templatePath = template;
+    this.templatePath = `${__dirname}/../templates/${template}.html`;
     this.template = null;
 
     this.structure = null;
     this.files = [];
-
-    this.run();
   }
 
   async readTemplate() {
@@ -26,14 +24,16 @@ class Generate {
       console.log(`Template file not found on '${this.templatePath}'`);
       return false;
     }
-    this.template = await fs.readFileSync(this.templatePath, {encoding: 'utf8'});
+    this.template = await fs.readFileSync(this.templatePath, { encoding: 'utf8' });
+
+    return this.template;
   }
 
   parseTemplate() {
     if (!this.template) return;
 
     const root = HTMLParser.parse(this.template, {
-      script: true
+      script: true,
     });
 
     this.structure = root.querySelector('structure');
@@ -41,63 +41,59 @@ class Generate {
     // Scripts should execute first because apply variables correctly.
     this.scripts = this.structure.querySelectorAll('script').map(item => {
       const Structure = {
-        getVariable: (name) => {
-          return this.variables[name];
-        },
+        getVariable: name => this.variables[name],
         passVariable: (name, value) => {
           this.variables[name] = value;
-        }
-      }
+        },
+      };
 
       eval(item.innerHTML);
 
       return item;
     });
 
-
     this.outputDirectory = this.applyVariables(this.structure.attributes.out);
     if (!this.outputDirectory) this.outputDirectory = '.';
 
-    this.files = this.structure.querySelectorAll('file').map(item => {
-      if (!item.attributes.name) return;
-
-      const file = new File(this.applyVariables(item.innerHTML));
-      file.name = this.applyVariables(item.attributes.name);
-      return file;
-    });
-
+    this.files = this.structure
+      .querySelectorAll('file')
+      .filter(item => !!item.attributes.name)
+      .map(item => {
+        const file = new File(item.innerHTML);
+        file.save(this.applyVariables(item.attributes.name));
+        return file;
+      });
   }
 
   applyVariables(content) {
-    for(let variable in this.variables) {
-      content = content.replace(new RegExp(this.variableTemplate.replace('var', variable), 'gi'), this.variables[variable]);
+    let contentWithVariables = content;
+    for (const variable in this.variables) {
+      contentWithVariables = contentWithVariables.replace(
+        new RegExp(this.variableTemplate.replace('var', variable), 'gi'),
+        this.variables[variable],
+      );
     }
-    return content;
+    return contentWithVariables;
   }
 
   createFiles() {
-    if (shell.test('-e', this.outputDirectory)) {
-      console.log(`Directory '${this.outputDirectory}' already exists.`);
-      return false;
+    if (!shell.test('-e', this.outputDirectory)) {
+      console.log(`Directory '${this.outputDirectory}' doesn't exist. Creating...`);
+      shell.mkdir('-p', this.outputDirectory);
     }
 
-    shell.mkdir('-p', this.outputDirectory);
-
-    const output = (name) => {
-      return `${this.outputDirectory}/${name}`;
-    }
+    const output = name => `${this.outputDirectory}/${name}`;
 
     this.files.map(file => {
       const out = output(file.name);
 
       if (shell.test('-e', out)) {
-        // I know this block is useless.
         console.log(`File '${file.name}' already exists. Skipped.`);
+      } else {
+        fs.writeFile(out, file.content, err => {
+          if (!err) console.log(`File: '${file.name}' created.`);
+        });
       }
-
-      fs.writeFile(out, file.content, (err) => {
-        if (!err) console.log(`File: '${file.name}' created.`)
-      });
     });
   }
 
@@ -106,7 +102,6 @@ class Generate {
     this.parseTemplate();
     this.createFiles();
   }
-
 }
 
 module.exports = Generate;
