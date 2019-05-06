4a8936538341b2fdb715ca92bf45ddff633bb4be
Tugay Baltacı <tugaybaltaci@gmail.com>
2019-05-06 11:17:07 +0300

    update cli tool according to Generator dependencies

--- bin/cli.js
+++ bin/cli.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
 const cli = require("command-line-args");
-const GenerateStructure = require("./core/Generator");
+const GenerateStructure = require("../core/Generator");
 
 const options = cli([
   { name: "name", type: String, defaultOption: true },
   { name: "templatePath", type: String, alias: "t" }
 ]);
 
-const structure = new GenerateStructure(options.name, options.template);
+const structure = new GenerateStructure(options.name, options.templatePath);
 structure.run();
